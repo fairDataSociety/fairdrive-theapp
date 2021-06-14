b3e0301ebe5349b4c3d288a7c50510c0bb8cc91d
Mustafa <mustafa@linumlabs.com>
2021-06-14 10:56:44 +0300

    update card grid styles and readme

--- app/src/components/register/registerStyles.ts
+++ app/src/components/register/registerStyles.ts
@@ -19,7 +19,7 @@ const useStyles = makeStyles(() =>
 
     },
     registerContainer:{
-      paddingLeft:"24rem",
+      paddingLeft:"7rem",
       display: "flex",
       flexDirection: "column",
       justifyItems: "center",
