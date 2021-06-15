70a47c969160f91b14c74d3bea919e19c8421cf2
Anzette <anzette@linumlabs.com>
2021-06-15 10:24:43 +0200

    Refactor and restyle search bar

---
+++ app/src/components/searchBar/searchBarStyles.ts
@@ -0,0 +1,44 @@
+import { makeStyles, createStyles } from '@material-ui/styles';
+import { Theme } from '../../store/themeContext/themes';
+import { Props } from './searchBar';
+
+const useStyles = makeStyles(() =>
+	createStyles({
+		searchBar: {
+			marginLeft: '2rem',
+			border: '1px solid var(--dark1)',
+			borderRadius: '0.5rem',
+		},
+		TextField: {
+			color: (style: Props & Theme) => style.textColorSecondary,
+			display: 'flex',
+			flexDirection: 'row',
+			alignItems: 'center',
+			padding: '1rem',
+			width: '45rem',
+		},
+		input: {
+			font: (style: Props & Theme) => style.typography.caption1,
+			color: (style: Props & Theme) => style.textColorSecondary,
+			textAlign: 'left',
+			width: '45rem',
+			border: '0px',
+			backgroundColor: 'transparent',
+			outline: 'none',
+		},
+		iconContainer: {
+			width: '2rem',
+			height: '2rem',
+			display: 'flex',
+			alignItems: 'center',
+			justifyContent: 'center',
+			marginRight: '1rem',
+		},
+		Icon: {
+			width: '2rem',
+			height: '2rem',
+		},
+	})
+);
+
+export default useStyles;
--- app/src/containers/navbar/navbar.tsx
+++ app/src/containers/navbar/navbar.tsx
@@ -3,8 +3,7 @@ import { ThemeContext } from "../../store/themeContext/themeContext";
 import { StoreContext } from "../../store/store";
 import useStyles from "./navbarStyles";
 import { Link } from "react-router-dom";
-import TextField from "../../components/textField/textField";
-import { Search } from "../../components/icons/icons";
+import SearchBar from '../../components/searchBar/searchBar';
 
 export interface Props {}
 
@@ -12,31 +11,14 @@ function Navbar(props: Props) {
   const { state, actions } = useContext(StoreContext);
   const { theme } = useContext(ThemeContext);
   const classes = useStyles({ ...props, ...theme });
-  const [searchQuery, setSearchQuery] = useState("");
-  const handleSetProp = (e: any) => {
-    actions.setSearchQuery(e.target.value);
-  };
 
   return (
     <div className={classes.Navbar}>
       <Link to={"/"} className={classes.logo}>
         Fairdrive
       </Link>
-      {state.userData && (
-        <div className={classes.searchBar}>
-          <div className={classes.TextField}>
-            <div className={classes.iconContainer}>
-              <Search className={classes.Icon} />
-            </div>
-
-            <input
-              className={classes.input}
-              type="text"
-              placeholder="Search"
-              onChange={(e) => handleSetProp(e)}
-            ></input>
-          </div>
-        </div>
+      {!state.userData && (
+        <SearchBar />
       )}
     </div>
   );
