baacfe9444f0337a364de6bf5768aea93dfb21fb
Anzette <anzette@linumlabs.com>
2021-06-17 14:33:05 +0200

    Connect to fairdrive modal

---
+++ app/src/components/modals/connectToFairdrive/connectToFairdrive.tsx
@@ -0,0 +1,25 @@
+import React, { useContext } from "react";
+import { ThemeContext } from "../../../store/themeContext/themeContext";
+import { StoreContext } from "../../../store/store";
+import useStyles from "./connectToFairdriveStyles";
+import Modal from "../modal/modal";
+
+export interface Props {}
+
+function ConnectToFairdrive(props: Props) {
+  const { state, actions } = useContext(StoreContext);
+  const { theme } = useContext(ThemeContext);
+
+  const classes = useStyles({ ...props, ...theme });
+
+  return (
+    <Modal heading='Connect to Fairdrive' button='Authorize'>
+      <p className={classes.label}>USERNAME</p>
+      <input className={classes.input} placeholder='Enter here...'/>
+      <p className={classes.label}>PASSWORD</p>
+      <input className={classes.input} placeholder='Enter here...'/>
+    </Modal>
+  );
+}
+
+export default React.memo(ConnectToFairdrive);
