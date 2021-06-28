a33cad9d5eba6dcff249a56d0b69d4e25d2569b2
Mustafa <mustafa@linumlabs.com>
2021-06-28 14:14:24 +0200

    added link to github issues

--- app/src/components/alertBanner/alertBanner.tsx
+++ app/src/components/alertBanner/alertBanner.tsx
@@ -1,31 +1,36 @@
-import React from 'react';
-import { useTheme } from '../../store/themeContext/themeContext';
-import useStyles from './alertBannerStyles';
-import { Warning } from '../icons/icons';
+import React from "react";
+import { useTheme } from "../../store/themeContext/themeContext";
+import useStyles from "./alertBannerStyles";
+import { Warning } from "../icons/icons";
 
 export interface Props {
-	setShowBanner: React.Dispatch<React.SetStateAction<boolean>>;
+  setShowBanner: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 function AlertBanner(props: Props) {
-	const { theme } = useTheme();
+  const { theme } = useTheme();
 
-	const classes = useStyles({ ...props, ...theme });
+  const classes = useStyles({ ...props, ...theme });
 
-	return (
-		<div className={classes.AlertBanner}>
-			<Warning className={classes.warningIcon} />
-			<div className={classes.betaWarningText}>
-				Fairdrive is in Beta and provided for evaluation only! File integrity,
-				persistence and security are not assured! Report Bugs.
-			</div>
-			<button
-				onClick={() => props.setShowBanner(false)}
-				className={classes.agree}>
-				Ok, I Agree!
-			</button>
-		</div>
-	);
+  return (
+    <div className={classes.AlertBanner}>
+      <Warning className={classes.warningIcon} />
+      <div className={classes.betaWarningText}>
+        Fairdrive is in Beta and provided for evaluation only! File integrity,
+        persistence and security are not assured!{" "}
+        <a href="https://github.com/fairDataSociety/fairdrive-theapp/issues">
+          Report Bugs
+        </a>
+        .
+      </div>
+      <button
+        onClick={() => props.setShowBanner(false)}
+        className={classes.agree}
+      >
+        Ok, I Agree!
+      </button>
+    </div>
+  );
 }
 
 export default React.memo(AlertBanner);
