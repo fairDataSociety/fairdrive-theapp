713f6d81bf18a0398b122ec282a1add5a5cb4ab5
Wojciech Kozioł <w.koziol@digitalcolliers.com>
2021-09-15 16:52:20 +0200

    Created almost perfect authMachine

--- app/src/machines/auth/index.ts
+++ app/src/machines/auth/index.ts
@@ -1,37 +1,57 @@
 import { createMachine, assign } from 'xstate';
-import STATES from './states';
-import EVENTS from './events';
+// import STATES from './states';
+// import EVENTS from './events';
 
 import { UserStats } from 'src/types/responses';
+import { CreateAccount } from 'src/types/models/CreateAccount';
 
 import * as LoginService from 'src/services/auth';
 import * as RegisterService from 'src/services/account';
 
+enum EVENTS {
+  LOGIN = 'login',
+  REGISTER = 'register',
+  LOGOUT = 'logout',
+}
+
+enum STATES {
+  STATE_ROOT = 'authMachine',
+  IDLE = 'idle',
+  LOGIN = 'login',
+  LOGIN_LOADING = 'login_loading',
+  LOGIN_SUCCESS = 'login_success',
+  LOGIN_FAILED = 'login_failed',
+  LOGOUT = 'logout',
+  REGISTER = 'register',
+  REGISTER_LOADING = 'register_loading',
+  REGISTER_SUCCESS = 'register_success',
+  REGISTER_FAILED = 'register_failed',
+}
+
+interface LoginData {
+  username: string;
+  password: string;
+}
 interface AuthContext {
   isLoggedIn: boolean;
   isRegistered: boolean;
   userData: UserStats | null;
   errorMessage: string | null;
+  loginData: LoginData | null;
+  registrationData: CreateAccount | null;
 }
 
-// TODO: Take value as value of STATES
-type AuthTypestate = 
-    {value: 'idle'; context: AuthContext} | 
-    {value: 'login'; context: AuthContext} |
-    {value: { login: 'loading' }; context: AuthContext } | 
-    {value:  { login: 'success' }; context: AuthContext } | 
-    {value:  { login: 'failure' }; context: AuthContext } |
-    {value:  { login: 'logout' }; context: AuthContext } |
-    {value: 'register'; context: AuthContext } | 
-    {value: { register: 'loading' }; context: AuthContext } | 
-    {value:  { register: 'success' }; context: AuthContext } | 
-    {value:  { register: 'failure' }; context: AuthContext };
+type AuthEvents =
+  | {
+      type: EVENTS.LOGIN;
+      payload: LoginData;
+    }
+  | { type: EVENTS.REGISTER; payload: CreateAccount }
+  | { type: EVENTS.LOGOUT; payload: undefined };
 
-
-
-const authMachine = createMachine<AuthContext, , AuthTypestate>({
-  id: 'authMachine',
-  initial: '',
+const createAuthMachine = createMachine<AuthContext, AuthEvents>({
+  id: STATES.STATE_ROOT,
+  initial: STATES.IDLE,
   context: {
     // TODO: Below it could by as services/user/isUserLoggedIn() ? 'login' : 'idle'
     // but currently we don't have properly working session refresh tool
@@ -39,110 +59,125 @@ const authMachine = createMachine<AuthContext, , AuthTypestate>({
     isRegistered: false,
     userData: null,
     errorMessage: null,
+    loginData: null,
+    registrationData: null,
   },
   states: {
     [STATES.IDLE]: {
       on: {
-        [EVENTS.REGISTER]: { target: STATES.REGISTER.NODE_NAME },
-        [EVENTS.LOGIN]: { target: STATES.LOGIN.NODE_NAME },
+        [EVENTS.LOGIN]: {
+          target: STATES.LOGIN,
+          actions: assign({
+            loginData: (_, { payload }) => payload,
+          }),
+        },
+        [EVENTS.REGISTER]: {
+          target: STATES.REGISTER_LOADING,
+          actions: assign({
+            registrationData: (_, { payload }) => payload,
+          }),
+        },
       },
     },
-    [STATES.REGISTER.NODE_NAME]: {
-      initial: STATES.REGISTER.LOADING,
+    [STATES.REGISTER]: {
+      initial: STATES.REGISTER_LOADING,
       states: {
-        [STATES.REGISTER.LOADING]: {
+        [STATES.REGISTER_LOADING]: {
           invoke: {
             id: 'registerService',
-            src: (_, event) => RegisterService.createAccount(event),
-            onDone: {
-              target: STATES.REGISTER.SUCCESS,
-              actions: assign({
-                isRegistered: true,
-              }),
-            },
-            onError: {
-              target: STATES.REGISTER.FAILURE,
-              actions: assign({
-                isRegistered: false,
-                error: (_, event) => {
-                  return event.data.message;
-                },
-              }),
-            },
+            src: (context) =>
+              RegisterService.createAccount(context.registrationData),
           },
         },
-        [STATES.REGISTER.SUCCESS]: {
+        [STATES.REGISTER_SUCCESS]: {
           on: {
+            // After register let's allow for login in
             [EVENTS.LOGIN]: {
-              target: `#authMachine.${STATES.LOGIN.NODE_NAME}.${STATES.LOGIN.LOADING}`,
+              target: `#${STATES.STATE_ROOT}.${STATES.LOGIN}.${STATES.LOGIN_LOADING}`,
+              actions: assign({
+                loginData: (_, { payload }) => payload,
+              }),
             },
           },
         },
-        [STATES.REGISTER.FAILURE]: {
+        [STATES.REGISTER_FAILED]: {
+          // On register failed let's allow for re try
           on: {
+            // After register let's allow for login in
             [EVENTS.REGISTER]: {
-              target: `#authMachine.${STATES.REGISTER.NODE_NAME}.${STATES.REGISTER.LOADING}`,
+              target: `#${STATES.STATE_ROOT}.${STATES.REGISTER}.${STATES.REGISTER_LOADING}`,
+              actions: assign({
+                registrationData: (_, { payload }) => payload,
+              }),
             },
           },
         },
       },
     },
-    [STATES.LOGIN.NODE_NAME]: {
-      initial: STATES.LOGIN.LOADING,
+    [STATES.LOGIN]: {
+      initial: STATES.LOGIN_LOADING,
       states: {
-        [STATES.LOGIN.LOADING]: {
+        [STATES.LOGIN_LOADING]: {
           invoke: {
             id: 'loginService',
-            src: (_, event) => LoginService.loginUser(event),
+            src: (context) => LoginService.loginUser(context.loginData),
             onDone: {
-              target: STATES.LOGIN.SUCCESS,
-              actions: assign({
-                isLoggedIn: true,
-              }),
+              target: STATES.LOGIN_SUCCESS,
+              actions: () => console.log('done'),
             },
             onError: {
-              target: STATES.LOGIN.FAILURE,
-              actions: assign({
-                isRegistered: false,
-                error: (_, event) => {
-                  return event.data.message;
-                },
-              }),
+              target: STATES.LOGIN_FAILED,
+              actions: () => console.log('error'),
             },
           },
         },
-        [STATES.LOGIN.FAILURE]: {
+        [STATES.LOGIN_SUCCESS]: {
+          // On login success let' allow for fetching user's object
+
           on: {
-            [EVENTS.LOGIN]: {
-              target: `#authMachine.${STATES.LOGIN.NODE_NAME}.${STATES.LOGIN.LOADING}`,
-            },
+            // On login success let's allow for log out
+            [EVENTS.LOGOUT]: { target: STATES.LOGOUT },
           },
         },
-        [STATES.LOGIN.LOGOUT]: {
-          invoke: {
-            id: 'logoutService',
-            src: () => LoginService.logoutUser(),
-            onDone: {
-              target: '#authMachine.idle',
-              actions: assign({
-                isLoggedIn: false,
-                isRegistered: false,
-                error: false,
-                user: null,
-              }),
-            },
-            onError: {
-              target: '#authMachine.idle',
-              actions: assign({
-                isLoggedIn: false,
-                isRegistered: false,
-                error: false,
-                user: null,
-              }),
-            },
+        [STATES.LOGIN_FAILED]: {
+          // When login fails let's allow for try once again
+          on: {
+            [EVENTS.LOGIN]: `#${STATES.STATE_ROOT}.${STATES.LOGIN}.${STATES.LOGIN_LOADING}`,
           },
+        },
+      },
+    },
+    [STATES.LOGOUT]: {
+      invoke: {
+        id: 'logoutService',
+        src: () => LoginService.logoutUser(),
+        onDone: {
+          // On logout move to initial state and context
+          target: `#${STATES.STATE_ROOT}.${STATES.IDLE}`,
+          actions: assign({
+            isLoggedIn: false,
+            isRegistered: false,
+            userData: null,
+            errorMessage: null,
+            loginData: null,
+            registrationData: null,
+          }),
+        },
+        onError: {
+          // On logout move to initial state and context
+          target: `#${STATES.STATE_ROOT}.${STATES.IDLE}`,
+          actions: assign({
+            isLoggedIn: false,
+            isRegistered: false,
+            userData: null,
+            errorMessage: null,
+            loginData: null,
+            registrationData: null,
+          }),
         },
       },
     },
   },
 });
+
+export default createAuthMachine;
