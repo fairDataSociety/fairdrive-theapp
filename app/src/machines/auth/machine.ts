4731c3faadbab78c6f190e55fb4c9a3c37e7ec48
Wojciech Kozioł <w.koziol@digitalcolliers.com>
2021-09-20 18:12:32 +0200

    Finished states: register, login, logout and fetch user stats

--- app/src/machines/auth/machine.ts
+++ app/src/machines/auth/machine.ts
@@ -36,6 +36,10 @@ export enum STATES {
   FETCH_USER_STATS_FAILED = 'fetch_userstats_failed',
 }
 
+enum GUARDS {
+  IS_USER_LOGGED = 'is_user_logged',
+}
+
 interface LoginData {
   username: string;
   password: string;
@@ -73,180 +77,190 @@ const initialState: AuthContext = {
   registrationData: null,
 };
 
-const createAuthMachine = createMachine<AuthContext, AuthEvents>({
-  id: STATES.STATE_ROOT,
-  initial: STATES.IDLE,
-  context: initialState,
-  states: {
-    [STATES.IDLE]: {
-      on: {
-        [EVENTS.LOGIN]: {
-          target: STATES.LOGIN,
-          actions: assign({
-            loginData: (_, { payload }) => payload,
-          }),
-        },
-        [EVENTS.REGISTER]: {
-          target: STATES.REGISTER,
-          actions: assign({
-            registrationData: (_, { payload }) => payload,
-          }),
-        },
-      },
-    },
-    [STATES.REGISTER]: {
-      initial: STATES.REGISTER_LOADING,
-      states: {
-        [STATES.REGISTER_LOADING]: {
-          invoke: {
-            id: 'registerService',
-            src: (context) =>
-              RegisterService.createAccount(context.registrationData),
-            onDone: {
-              target: STATES.REGISTER_SUCCESS,
-              actions: () => console.log('registerService done'),
-            },
-            onError: {
-              target: STATES.REGISTER_FAILED,
-              actions: () => console.log('registerService error'),
-            },
+const createAuthMachine = createMachine<AuthContext, AuthEvents>(
+  {
+    id: STATES.STATE_ROOT,
+    initial: STATES.IDLE,
+    context: initialState,
+    states: {
+      [STATES.IDLE]: {
+        on: {
+          [EVENTS.LOGIN]: {
+            target: STATES.LOGIN,
+            actions: assign({
+              loginData: (_, { payload }) => payload,
+            }),
           },
-        },
-        [STATES.REGISTER_SUCCESS]: {
-          on: {
-            // After register let's allow for login in
-            [EVENTS.LOGIN]: {
-              target: `#${STATES.STATE_ROOT}.${STATES.LOGIN}.${STATES.LOGIN_LOADING}`,
-              actions: assign({
-                loginData: (_, { payload }) => payload,
-              }),
-            },
-          },
-        },
-        [STATES.REGISTER_FAILED]: {
-          // On register failed let's allow for re try
-          on: {
-            // After register let's allow for login in
-            [EVENTS.REGISTER]: {
-              target: `#${STATES.STATE_ROOT}.${STATES.REGISTER}.${STATES.REGISTER_LOADING}`,
-              actions: assign({
-                registrationData: (_, { payload }) => payload,
-              }),
-            },
+          [EVENTS.REGISTER]: {
+            target: STATES.REGISTER,
+            actions: assign({
+              registrationData: (_, { payload }) => payload,
+            }),
           },
         },
       },
-    },
-    [STATES.LOGIN]: {
-      initial: STATES.LOGIN_LOADING,
-      states: {
-        [STATES.LOGIN_LOADING]: {
-          invoke: {
-            id: 'loginService',
-            src: (context) => LoginService.loginUser(context.loginData),
-            onDone: {
-              target: STATES.LOGIN_SUCCESS,
-              actions: assign({
-                isLoggedIn: (_) => true,
-              }),
-            },
-            onError: {
-              target: STATES.LOGIN_FAILED,
-              actions: assign({
-                isLoggedIn: (_) => false,
-              }),
-            },
-          },
-        },
-        [STATES.LOGIN_SUCCESS]: {
-          initial: 'idle',
-          states: {
-            idle: {
-              on: {
-                [EVENTS.FETCH_USER_STATS]: {
-                  target: STATES.FETCH_USER_STATS,
-                },
+      [STATES.REGISTER]: {
+        initial: STATES.REGISTER_LOADING,
+        states: {
+          [STATES.REGISTER_LOADING]: {
+            invoke: {
+              id: 'registerService',
+              src: (context) =>
+                RegisterService.createAccount(context.registrationData),
+              onDone: {
+                target: STATES.REGISTER_SUCCESS,
+                actions: () => console.log('registerService done'),
+              },
+              onError: {
+                target: STATES.REGISTER_FAILED,
+                actions: () => console.log('registerService error'),
               },
             },
-            [STATES.FETCH_USER_STATS]: {
-              // Below probably should be full reference
-              initial: STATES.FETCH_USER_STATS_LOADING,
-              states: {
-                [STATES.FETCH_USER_STATS_LOADING]: {
-                  invoke: {
-                    id: 'fetchUserStatsService',
-                    src: (_) => statsUser(),
-                    onDone: {
-                      target: STATES.FETCH_USER_STATS_SUCCESS,
-                      actions: assign((_, event) => {
-                        return {
-                          isUserStatsFetched: true,
-                          userData: event.data.data,
-                        };
-                      }),
-                    },
-                    onError: {
-                      target: STATES.FETCH_USER_STATS_FAILED,
-                      actions: assign((_) => {
-                        return {
-                          isUserStatsFetched: false,
-                          userData: null,
-                        };
-                      }),
-                    },
-                  },
-                },
-                // End of path
-                [STATES.FETCH_USER_STATS_SUCCESS]: {},
-                [STATES.FETCH_USER_STATS_FAILED]: {
-                  on: {
-                    [EVENTS.RETRY_FETCH_USER_STATS]: {
-                      target: STATES.FETCH_USER_STATS_LOADING,
-                    },
+          },
+          [STATES.REGISTER_SUCCESS]: {
+            on: {
+              // After register let's allow for login in
+              [EVENTS.LOGIN]: {
+                target: `#${STATES.STATE_ROOT}.${STATES.LOGIN}.${STATES.LOGIN_LOADING}`,
+                actions: assign({
+                  loginData: (_, { payload }) => payload,
+                }),
+              },
+            },
+          },
+          [STATES.REGISTER_FAILED]: {
+            // On register failed let's allow for re try
+            on: {
+              // After register let's allow for login in
+              [EVENTS.REGISTER]: {
+                target: `#${STATES.STATE_ROOT}.${STATES.REGISTER}.${STATES.REGISTER_LOADING}`,
+                actions: assign({
+                  registrationData: (_, { payload }) => payload,
+                }),
+              },
+            },
+          },
+        },
+      },
+      [STATES.LOGIN]: {
+        initial: STATES.LOGIN_LOADING,
+        states: {
+          [STATES.LOGIN_LOADING]: {
+            invoke: {
+              id: 'loginService',
+              src: (context) => LoginService.loginUser(context.loginData),
+              onDone: {
+                target: STATES.LOGIN_SUCCESS,
+                actions: assign({
+                  isLoggedIn: (_) => true,
+                }),
+              },
+              onError: {
+                target: STATES.LOGIN_FAILED,
+                actions: assign({
+                  isLoggedIn: (_) => false,
+                }),
+              },
+            },
+          },
+          [STATES.LOGIN_SUCCESS]: {
+            initial: 'idle',
+            states: {
+              idle: {
+                on: {
+                  [EVENTS.FETCH_USER_STATS]: {
+                    target: STATES.FETCH_USER_STATS,
+                    // Prevent fetching user stats when user not logged in
+                    cond: GUARDS.IS_USER_LOGGED,
                   },
                 },
               },
-              on: {
-                // On fetching user stats success let's allow for log out
-                [EVENTS.LOGOUT]: {
-                  target: `#${STATES.STATE_ROOT}.${STATES.LOGOUT}`,
+              [STATES.FETCH_USER_STATS]: {
+                // Below probably should be full reference
+                initial: STATES.FETCH_USER_STATS_LOADING,
+                states: {
+                  [STATES.FETCH_USER_STATS_LOADING]: {
+                    invoke: {
+                      id: 'fetchUserStatsService',
+                      src: (_) => statsUser(),
+                      onDone: {
+                        target: STATES.FETCH_USER_STATS_SUCCESS,
+                        actions: assign((_, event) => {
+                          return {
+                            isUserStatsFetched: true,
+                            userData: event.data.data,
+                          };
+                        }),
+                      },
+                      onError: {
+                        target: STATES.FETCH_USER_STATS_FAILED,
+                        actions: assign((_) => {
+                          return {
+                            isUserStatsFetched: false,
+                            userData: null,
+                          };
+                        }),
+                      },
+                    },
+                  },
+                  // End of path
+                  [STATES.FETCH_USER_STATS_SUCCESS]: {},
+                  [STATES.FETCH_USER_STATS_FAILED]: {
+                    on: {
+                      [EVENTS.RETRY_FETCH_USER_STATS]: {
+                        target: STATES.FETCH_USER_STATS_LOADING,
+                      },
+                    },
+                  },
+                },
+                on: {
+                  // On fetching user stats success let's allow for log out
+                  [EVENTS.LOGOUT]: {
+                    target: `#${STATES.STATE_ROOT}.${STATES.LOGOUT}`,
+                  },
                 },
               },
             },
-          },
-          on: {
-            // On login success let's allow for log out
-            [EVENTS.LOGOUT]: {
-              target: `#${STATES.STATE_ROOT}.${STATES.LOGOUT}`,
-            },
-          },
-        },
-        [STATES.LOGIN_FAILED]: {
-          // When login fails let's allow for try once again
-          on: {
-            [EVENTS.LOGIN]: `#${STATES.STATE_ROOT}.${STATES.LOGIN}.${STATES.LOGIN_LOADING}`,
-          },
-        },
-      },
-    },
 
-    [STATES.LOGOUT]: {
-      invoke: {
-        id: 'logoutService',
-        src: () => LoginService.logoutUser(),
-        onDone: {
-          // On logout move to initial state and context
-          target: `#${STATES.STATE_ROOT}.${STATES.IDLE}`,
-          actions: assign(initialState),
+            on: {
+              // On login success let's allow for log out
+              [EVENTS.LOGOUT]: {
+                target: `#${STATES.STATE_ROOT}.${STATES.LOGOUT}`,
+              },
+            },
+          },
+          [STATES.LOGIN_FAILED]: {
+            // When login fails let's allow for try once again
+            on: {
+              [EVENTS.LOGIN]: `#${STATES.STATE_ROOT}.${STATES.LOGIN}.${STATES.LOGIN_LOADING}`,
+            },
+          },
         },
-        onError: {
-          // On logout move to initial state and context
-          target: `#${STATES.STATE_ROOT}.${STATES.IDLE}`,
-          actions: assign(initialState),
+      },
+
+      [STATES.LOGOUT]: {
+        invoke: {
+          id: 'logoutService',
+          src: () => LoginService.logoutUser(),
+          onDone: {
+            // On logout move to initial state and context
+            target: `#${STATES.STATE_ROOT}.${STATES.IDLE}`,
+            actions: assign(initialState),
+          },
+          onError: {
+            // On logout move to initial state and context
+            target: `#${STATES.STATE_ROOT}.${STATES.IDLE}`,
+            actions: assign(initialState),
+          },
         },
       },
     },
   },
-});
+  {
+    guards: {
+      [GUARDS.IS_USER_LOGGED]: ({ isLoggedIn }) => isLoggedIn,
+    },
+  }
+);
 
 export default createAuthMachine;
