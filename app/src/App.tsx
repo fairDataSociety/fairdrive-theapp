bb60a35103f2355eab2510bb27c451a9d77ec21d
Wojciech Kozioł <w.koziol@digitalcolliers.com>
2021-09-23 14:43:48 +0200

    Removed old store

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import AuthProvider from 'src/machines/auth';
 import PodProvider from 'src/machines/pod';
 import FileProvider from 'src/machines/file';
-import { StoreProvider } from 'src/store/store';
-import { PodStateMachineProvider } from 'src/contexts/podStateMachine';
 import { ThemeProvider } from 'src/contexts/themeContext/themeContext';
 import Layout from 'src/layout/layout';
 import { Toaster } from 'react-hot-toast';
@@ -23,13 +21,9 @@ const App = (): JSX.Element => {
       <AuthProvider>
         <PodProvider>
           <FileProvider>
-            <PodStateMachineProvider>
-              <StoreProvider>
-                <ThemeProvider>
-                  <Layout />
-                </ThemeProvider>
-              </StoreProvider>
-            </PodStateMachineProvider>
+            <ThemeProvider>
+              <Layout />
+            </ThemeProvider>
           </FileProvider>
         </PodProvider>
       </AuthProvider>
