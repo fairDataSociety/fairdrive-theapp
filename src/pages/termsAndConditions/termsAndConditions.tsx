f0408cb795c08dbf9127ce51a5e0333cd6601a47
Dominic Mostert <mostertcoder@gmail.com>
2021-12-13 17:26:26 +0200

    Update Terms and Conditions

--- src/pages/termsAndConditions/termsAndConditions.tsx
+++ src/pages/termsAndConditions/termsAndConditions.tsx
@@ -59,12 +59,45 @@ function TermsAndConditions() {
         Disclosing Party.
       </p>
 
-      <h3 className={classes.title}>Privacy</h3>
+      <h3 className={classes.title}>Analytics - Matomo</h3>
       <p className={classes.paragraph}>
-        Fairdrive platform collects no data of its users whatsoever. It traces
-        no cookies and makes use of no analytical tools. The privacy policy
-        applied by Fair Data Society and thus Fairdrive platform is radical
-        minimization of data.
+        Fairdrive website and app uses the open source and self-hosted web
+        analysis service Matomo. Matomo uses technologies that make it possible
+        to recognize the user across multiple pages with the aim of analyzing
+        the user patterns (e.g. cookies or device fingerprinting). The
+        information recorded by Matomo about the use of this website will be
+        stored on our server. Prior to archiving, the IP address will first be
+        anonymized.
+        <br />
+        <br />
+        Through Matomo, we are able to collect and analyze data on the use of
+        our website by website visitors. This enables us to find out, for
+        instance, when which page views occurred and from which region they
+        came. In addition, we collect various log files (e.g. IP address,
+        referrer, browser and operating system used) and can measure whether our
+        website visitors perform certain actions (e.g. clicks, etc.).
+        <br />
+        <br />
+        The use of this analysis tool is based on Art. 6 Sect. 1 lit. f GDPR.
+        The website operator has a legitimate interest in the analysis of user
+        patterns, in order to optimize the operator’s web offerings and
+        advertising. If a corresponding agreement has been requested (e.g. an
+        agreement to the storage of cookies), the processing takes place
+        exclusively on the basis of Art. 6 para. 1 lit. a GDPR; the agreement
+        can be revoked at any time.
+        <br />
+        <br />
+        Completion of a data processing agreement We host Matomo on servers in
+        Switzerland, so that all the data and analysis remains with us and is
+        not passed on. The servers are provided by Exoscale and their Data
+        Protection Agreement can be viewed at:
+        <a
+          href="https://www.exoscale.com/dpa/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          https://www.exoscale.com/dpa/
+        </a>
       </p>
       <p className={classes.paragraph}>
         You agree and acknowledge that these Terms and Conditions may change
