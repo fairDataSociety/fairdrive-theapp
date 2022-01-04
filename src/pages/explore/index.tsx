3b5e46396e766ddab8ef667c4037c47e84c5641f
Dominic Mostert <mostertcoder@gmail.com>
2022-01-04 14:54:03 +0200

    Add Explore Page Heading

--- src/pages/explore/index.tsx
+++ src/pages/explore/index.tsx
@@ -3,7 +3,6 @@ import { FC } from 'react';
 import dapps from '@data/explore-dapps.json';
 
 import { MainLayout } from '@components/Layouts';
-// import { MainHeader } from '@components/Headers';
 import { ExploreCard } from '@components/Cards';
 
 interface ExploreProps {}
@@ -11,9 +10,11 @@ interface ExploreProps {}
 const Explore: FC<ExploreProps> = () => {
   return (
     <MainLayout>
-      {/* <MainHeader title="Explore DApps" /> */}
+      <h2 className="font-semibold text-xl text-color-accents-plum-black dark:text-color-shade-white-night">
+        Explore DApps
+      </h2>
 
-      <div className="grid grid-cols-5 gap-6">
+      <div className="mt-10 grid grid-cols-5 gap-6">
         {dapps.map((dapp) => {
           return (
             <ExploreCard
