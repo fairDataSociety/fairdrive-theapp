6ffe0ab025ef77a90cf75c0eb30434c597ea405e
James Olinya <olinyajames@gmail.com>
2022-06-21 14:30:25 +0100

    add fdp storage provider to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,20 +7,23 @@ import { SearchProvider } from '@context/SearchContext';
 import { PodProvider } from '@context/PodContext';
 
 import '@styles/globals.scss';
+import { FdpStorageProvider } from '@context/FdpStorageContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Matomo>
-      <ThemeProvider>
-        <UserProvider>
-          <SearchProvider>
-            <PodProvider>
-              <Component {...pageProps} />
-            </PodProvider>
-          </SearchProvider>
-        </UserProvider>
-      </ThemeProvider>
-    </Matomo>
+    <FdpStorageProvider>
+      <Matomo>
+        <ThemeProvider>
+          <UserProvider>
+            <SearchProvider>
+              <PodProvider>
+                <Component {...pageProps} />
+              </PodProvider>
+            </SearchProvider>
+          </UserProvider>
+        </ThemeProvider>
+      </Matomo>
+    </FdpStorageProvider>
   );
 }
 
