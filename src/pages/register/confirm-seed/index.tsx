08dd60610b62c5bd6d279410958ca92a497d4bbc
James Olinya <olinyajames@gmail.com>
2022-07-12 12:23:56 +0100

    redirect to register page if mnemonic doesn't exist in confirm mnemonic page

--- src/pages/register/confirm-seed/index.tsx
+++ src/pages/register/confirm-seed/index.tsx
@@ -2,8 +2,25 @@ import type { NextPage } from 'next';
 
 import { AuthenticationLayout } from '@components/Layouts';
 import { ConfirmMnemonic } from '@components/Forms';
+import { useFdpStorage } from '@context/FdpStorageContext';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 const RegisterConfirmSeed: NextPage = () => {
+  const { mnemonic } = useFdpStorage();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!mnemonic) {
+      router.push('/register');
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (!mnemonic) {
+    return <></>;
+  }
+
   return (
     <AuthenticationLayout>
       <ConfirmMnemonic />
