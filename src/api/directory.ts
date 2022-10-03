d81e2205f85cf4138b944ef25a53b85b212cf243
Rogelio Morrell <molekilla@gmail.com>
2022-10-03 12:09:20 -0500

    feat: normalize directories (root or child)

--- src/api/directory.ts
+++ src/api/directory.ts
@@ -1,25 +1,22 @@
 import { FdpStorage } from '@fairdatasociety/fdp-storage';
 
+export async function createDefaultDirectory(fdp: FdpStorage, podName: string) {
+  return createDirectory(fdp, podName, '/', '');
+}
+
 export async function createDirectory(
   fdp: FdpStorage,
   podName: string,
   directory: string,
   directoryName: string
 ) {
-  // Dir = "/" + path + "/"
-  // TODO write helper for this piece of code
-
   let data = { dir_path: '' };
-
   if (directory === 'root') {
-    data = {
-      dir_path: '/' + directoryName,
-    };
-  } else {
-    data = {
-      dir_path: '/' + directory + '/' + directoryName,
-    };
+    directory = `/`;
   }
+  data = {
+    dir_path: `${directory}${directoryName}`,
+  };
   try {
     await fdp.directory.create(podName, data.dir_path);
 
