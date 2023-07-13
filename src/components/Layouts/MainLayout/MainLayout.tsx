da2df3d5186288f51df0ac6479b9b9c7f66467f4
Vladan <tomic.vladan.mail@gmail.com>
2023-07-13 12:04:15 +0200

    fix: increase animation speed

--- src/components/Layouts/MainLayout/MainLayout.tsx
+++ src/components/Layouts/MainLayout/MainLayout.tsx
@@ -77,6 +77,9 @@ const MainLayout: FC<MainLayoutProps> = ({
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 exit={{ opacity: 0 }}
+                transition={{
+                  duration: 0.1,
+                }}
               >
                 {children}
               </motion.div>
