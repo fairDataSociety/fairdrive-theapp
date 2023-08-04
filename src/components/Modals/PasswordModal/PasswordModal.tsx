dd70d0989d99358eec0ba3bc55ebe376bcce2b64
Igor Shadurin <igor.shadurin@gmail.com>
2023-08-04 11:52:22 +0300

    refactor: changed text

--- src/components/Modals/PasswordModal/PasswordModal.tsx
+++ src/components/Modals/PasswordModal/PasswordModal.tsx
@@ -42,13 +42,9 @@ const PasswordModal: FC<PasswordModalProps> = ({
     <Modal
       showModal={showModal}
       closeModal={closeModal}
-      headerTitle="Enter password"
+      headerTitle="Enter passphrase"
     >
       <form onSubmit={handleSubmit(handleSubmitButton)} className="w-full">
-        <p className="text-sm mb-4">
-          Your passphrase is used for the encryption and decryption of your
-          data. Do not share it.
-        </p>
         <AuthenticationInput
           label=""
           id="password"
@@ -65,6 +61,11 @@ const PasswordModal: FC<PasswordModalProps> = ({
           }}
           error={errors.password as FieldError}
         />
+
+        <p className="text-sm mb-5">
+          Your passphrase is used for the encryption and decryption of your
+          data. Do not share it.
+        </p>
 
         <div className="text-center">
           <Button
