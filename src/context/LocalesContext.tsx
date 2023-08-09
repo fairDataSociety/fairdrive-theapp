09563d98a977062559dcd148f485ba474ebcac99
Vladan <tomic.vladan.mail@gmail.com>
2023-08-09 14:29:31 +0200

    fix: add other languages

--- src/context/LocalesContext.tsx
+++ src/context/LocalesContext.tsx
@@ -2,7 +2,18 @@
 import React, { useMemo, useState } from 'react';
 import { createContext, useContext } from 'react';
 import intl from 'react-intl-universal';
+import cnCnLocales from '../locales/ch-CH.json';
+import deDeLocales from '../locales/de-DE.json';
 import enUsLocales from '../locales/en-US.json';
+import esEsLocales from '../locales/es-ES.json';
+import frFrLocales from '../locales/fr-FR.json';
+import huHuLocales from '../locales/hu-HU.json';
+import itItLocales from '../locales/it-IT.json';
+import jpJpLocales from '../locales/jp-JP.json';
+import prPtLocales from '../locales/pt-PT.json';
+import rsSrLocales from '../locales/rs-RS.json';
+import slSiLocales from '../locales/sl-SI.json';
+import trTrLocales from '../locales/tr-TR.json';
 
 import cnFlag from '../media/flags/cn.png';
 import deFlag from '../media/flags/de.png';
@@ -69,7 +80,18 @@ function setLanguage(language: string) {
   intl.init({
     currentLocale: language,
     locales: {
+      'ch-CH': cnCnLocales,
+      'de-DE': deDeLocales,
       'en-US': enUsLocales,
+      'es-ES': esEsLocales,
+      'fr-FR': frFrLocales,
+      'hu-HU': huHuLocales,
+      'it-IT': itItLocales,
+      'jp-JP': jpJpLocales,
+      'pt-PT': prPtLocales,
+      'rs-RS': rsSrLocales,
+      'sl-SI': slSiLocales,
+      'tr-TR': trTrLocales,
     },
   });
 }
